0a81809136adf9869e8b01029ea92ee2b14dc8a6
saurabh sharma <saurabhcoded@gmail.com>
2025-03-26 11:41:31 +0000

    Fix confirmly folder issue

--- lib/index.ts
+++ lib/index.ts
@@ -1,5 +1,5 @@
 export { Notify as notify } from './Notify';
 export { useConfirmly, useConfirmlyContext } from './hooks';
 export { ConfirmlyProvider } from './ConfirmlyProvider';
-export { confirmly } from './confirmly';
+export { confirmly } from './Confirmly';
 export * as Types from './types';
